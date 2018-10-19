143337528ef7926674a0e54ed69a3cfbbcb25007
Chukwuemeka Dan-Chuku <chukwuemekadanchuku@gmail.com>
2018-10-19 20:15:26 +0100

    updated the app.py for backend functionalities

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
 from flask import Flask, render_template
+import mysql.connector as mariadb
+import requests
+from envparse import env
+from nytdatabase import DataBase
+from datetime import datetime
+
+
+env.read_envfile()
+nytkey = env.str('nytapikey')
+
+ny_database=DataBase()
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+
+    payload = {'api-key': nytkey}
+
+    r = requests.get('https://api.nytimes.com/svc/topstories/v2/home.json', params=payload)
+
+    render_data = []
+    data = r.json()["results"]
+    for i, value in enumerate(data):
+        if i<5:
+            print(data[i])
+            d = {}
+            d['title'] = data[i]['title']
+            d['byline'] = data[i]['byline']
+            d['url'] = data[i]['url']
+            d['published_date'] = data[i]['published_date']
+
+            render_data.append(d)
+
+    #capture data from the api to the database
+    ny_database.insert_data(render_data,datetime.now())
+    #print(render_data)
+    return render_template('home.html', render_data= render_data)
+
 
 @app.route('/about/')
 def about():
